04e29eef282dbd00dcff68d0f2fc1f02a3b57f81
Danny Yebs <danny.yebs@gmail.com>
2024-05-17 12:55:23 +0000

    Modified the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import LoginForm from "./login-form";
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
 
 export default async function LoginPage() {
   const session = await getServerSession();
@@ -11,8 +13,17 @@ export default async function LoginPage() {
   }
 
   return (
-    <section className="bg-black h-screen flex items-center justify-center">
-      <div className="">
+    <section className="bg-gradient-to-r from-purple-500 to-purple-900 h-screen flex items-center justify-center">
+      <div className=" flex flex-col items-center justify-center gap-2">
+        <Card className="p-4 rounded-full shadow-lg shadow-black">
+          <Image 
+            src="/ucc-logo.webp"
+            height={50}
+            width={50} 
+            alt={"UCC Logo"}          
+            className=""
+            />
+        </Card>
         <LoginForm />;
       </div>
     </section>
